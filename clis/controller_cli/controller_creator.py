ab4178be8e3a21ddab038a9b0f7fa3c219f0882a
waroma <razer30606@gmail.com>
2023-12-20 15:58:17 +0300

    CLEAN UP

--- clis/controller_cli/controller_creator.py
+++ clis/controller_cli/controller_creator.py
@@ -44,7 +44,8 @@ class ControllerCreator:
             path_manager.mkdir()
 
         # Handle files
-        mandatory_files = self.controller_constants.get_mandatory_files()+self.controller_constants.get_optional_files()
+        mandatory_files = self.controller_constants.get_mandatory_files() +\
+                          self.controller_constants.get_optional_files()
         new_formatted_files = []
         print(mandatory_files)
         for mandatory_file in mandatory_files:
@@ -56,14 +57,13 @@ class ControllerCreator:
                                 {blank}
                                 ```""".format(path=mandatory_file["path"], blank=mandatory_file["content"])
             file = chain.run(format_instructions=parser.get_format_instructions(),
-                                formatted_blank=formatted_blank,
-                                description=description_prompt,
-                                controller_name=controller_name,
-                                previous_files_created='\n'.join(new_formatted_files))
+                             formatted_blank=formatted_blank,
+                             description=description_prompt,
+                             controller_name=controller_name,
+                             previous_files_created='\n'.join(new_formatted_files))
             output = parser.parse(file)
 
             new_formatted_files.append(formatted_blank)
-            print(file)
             self.create_file(mandatory_file["path"], '\n'.join(output))
             yield mandatory_file['name'], '\n'.join(output)
 
@@ -79,7 +79,7 @@ class ControllerCreator:
 
         for optional_file in optional_files:
             self.create_file(optional_file["path"], optional_file["content"])
-            yield optional_file['name']
+            yield optional_file['name'], optional_file["content"]
 
     def create_file(self, fpath: str, contents: str):
         with open(fpath, "w") as f:
