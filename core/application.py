c2fb4263703d3b7f82ce774b3f3c94a6166f073d
Zahgrom34 <Zakhriw@gmail.com>
2024-06-23 17:31:01 +0500

    Version Patch updates

--- core/application.py
+++ core/application.py
@@ -88,7 +88,7 @@ class Application:
             raise e
     
     def get_version(self) -> str:
-        return "v1.1.0"
+        return "v1.1.1"
     
     def use_authentication(self, token_url: str = "/auth/login"):
         AscenderAuthenticationFramework.run_authentication(self, token_url=token_url)
