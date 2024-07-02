6dc13b7faf165f9c7d302225923cbea0adf74c52
Zahgrom34 <Zakhriw@gmail.com>
2024-07-02 14:02:17 +0500

    Dependencies update

--- settings.py
+++ settings.py
@@ -3,7 +3,8 @@ This file is used to configure the ORM settings & API Configurations.
 Please do not store sensitive information in this file, this file is not for storing tokens and secret keys.
 """
 import os
-from core.extensions.authentication import AscenderAuthenticationFramework
+
+from fastapi.security import HTTPBearer
 
 TORTOISE_ORM = {
     "connections": {
@@ -16,10 +17,14 @@ TORTOISE_ORM = {
     },
     "apps": {
         "models": {
-            "models": [AscenderAuthenticationFramework.use_authentication_database(), "aerich.models"],  # Replace with the path to your models module
+            "models": ["entities.test", "aerich.models"],  # Replace with the path to your models module
             "default_connection": "default"
         }
     }
+}
+DATABASE_CONNECTION = {
+    "type": "dbstring",
+    "content": "sqlite+aiosqlite:///database.db"
 }
 ORIGINS = [
     "*",
@@ -28,3 +33,4 @@ ORIGINS = [
 BASE_PATH = os.path.dirname(os.path.abspath(__file__))
 HEADERS = None
 PLUGINS_LOGLEVEL = "INFO"
+AUTHORIZATION_SCHEME = HTTPBearer()
