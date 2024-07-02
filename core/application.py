b3ac7b72ac4881f1a01dcd39a6ab56e72ec4e693
Zahgrom34 <Zakhriw@gmail.com>
2024-07-02 13:57:02 +0500

    Update application.py

--- core/application.py
+++ core/application.py
@@ -1,18 +1,25 @@
-from core.cli_apps.controller_cli.cli_processor import ControllerCLI
+from core.cli_apps.controllers_manager.app import ControllersManagerCLI
 from core.cli_apps.migrate_cli import MigrateCLI
 from core.cli_apps.users_cli import UsersCLI
+from core.database.engine import DatabaseEngine
+from core.database.types.orm_enum import ORMEnum
 from core.extensions.authentication import AscenderAuthenticationFramework
 from importlib import import_module
 from fastapi import FastAPI
-from typing import Any, Callable, List
+from typing import Any, Callable, List, Literal
 
 import uvicorn
 from core.cli.processor import CLI
 from core.cli_apps.serve_cli import BuildControlCLI, ServerControlCLI
 from core.extensions.authentication.custom.provider import AuthenticationProvider
+from core.extensions.repositories import IdentityRepository
+from core.identity.manager import IdentityManager
+from core.identity.security import Security
+from core.identity.singleton import SecuritySingleton
 from core.loader import Loader
 from core.plugins.plugin import Plugin
 from core.plugins.plugin_loader import PluginLoader
+from core.registries.service import ServiceRegistry
 from core.sockets import SocketIOApp
 
 from core.types import Controller
@@ -30,6 +37,7 @@ class Application:
         self.app = FastAPI(title="Ascender Framework API")
         self.socketio: SocketIOApp | None = None
 
+        self.service_registry = ServiceRegistry()
         self._on_server_start = on_server_start
         self._on_server_runtime_error = on_server_runtime_error
         self._on_cli_run = on_cli_run
@@ -41,6 +49,7 @@ class Application:
         self._plugin_loader = PluginLoader(self)
 
         self.loader_module = Loader(self.app, self, controllers, self._plugin_loader)
+        self.security = None
         
         # Initialize CLI module
         self.__cli = CLI(self, app_name="AscCLI")
@@ -48,10 +57,13 @@ class Application:
     def test_callback(self, *args, **kwargs):
         print(*args, **kwargs)
 
-    def use_database(self):
-        module = import_module("core.database")
+    def use_database(self, logical_o: Callable[[DatabaseEngine], None], orm: ORMEnum, configuration: dict):
+        # module = import_module("core.database")
 
-        module.run_database(self.app)
+        # module.run_database(self.app)
+        database = DatabaseEngine(self.app, orm, configuration)
+        self.service_registry.add_singletone(DatabaseEngine, database)
+        logical_o(database)
     
     async def use_database_cli(self):
         module = import_module("core.database")
@@ -66,12 +78,12 @@ class Application:
         if self._on_cli_run is not None:
             self._on_cli_run(self, self.__cli)
 
-        self.__cli.register_base("serve", ServerControlCLI())
-        self.__cli.register_base("build", BuildControlCLI())
+        self.__cli.register_base("serve", ServerControlCLI)
+        self.__cli.register_base("build", BuildControlCLI)
 
-        self.__cli.register_generic(UsersCLI())
-        self.__cli.register_generic(MigrateCLI())
-        self.__cli.register_generic(ControllerCLI())
+        # self.__cli.register_generic(UsersCLI)
+        self.__cli.register_generic(MigrateCLI)
+        self.__cli.register_generic(ControllersManagerCLI)
         self.__cli.run()
     
     def run_server(self, host: str, port: int) -> Callable | None:
@@ -96,6 +108,16 @@ class Application:
     def use_custom_authentication(self, auth_provider: AuthenticationProvider):
         AscenderAuthenticationFramework.run_custom_authentication(self, auth_provider)
 
+    def add_authorization(self, logical_o: Callable[[Security], None],
+                          identity_repository: IdentityRepository,
+                          auth_scheme: Literal["basic", "apikey", "oauth2", "cookie"] = "basic",
+                          *scheme_args, **scheme_kargs):
+        self.security = SecuritySingleton(Security(identity_repository=identity_repository,
+                                 auth_scheme=auth_scheme,
+                                 *scheme_args, **scheme_kargs))
+        self.service_registry.add_singletone(IdentityManager, IdentityManager(self.security.engine))
+        logical_o(self.security)
+
     def use_sio(self, **options):
         """
         ## Use SocketIO
@@ -113,5 +135,5 @@ class Application:
             if self._on_injections_run is not None:
                 self._on_injections_run(self)
             self._on_server_start(self)
-        
+        self.app.add_event_handler("startup", self.loader_module.initialize_dependencies)
         return self.app
